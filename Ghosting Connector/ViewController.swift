21614afc6be8d0f477144a928d952a02606c541f
Varun Chitturi <varunchitturi@icloud.com>
2020-04-25 13:47:50 -0700

    Added a switch and image control to the switch

--- Ghosting Connector/ViewController.swift
+++ Ghosting Connector/ViewController.swift
@@ -13,8 +13,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mainToggle.setOn(false, animated: true)
+        lightImage.image = UIImage(systemName: "lightbulb")
+        
+}
+
+    
+
+    @IBOutlet weak var mainToggle: UISwitch!
+    
+    
+    @IBAction func toggle(_ sender: Any) {
+        if mainToggle.isOn{
+    lightImage.image = UIImage(systemName: "lightbulb.fill")
+       }
+    else{
+        lightImage.image = UIImage(systemName: "lightbulb")
+        }
     }
-
-
+    
+    
+   
+    
+  
+    
+    @IBOutlet weak var lightImage: UIImageView!
+    
 }
 
