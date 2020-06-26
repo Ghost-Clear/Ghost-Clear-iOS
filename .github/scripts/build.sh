f2c06431ef19ffed172e63514b13a397948daa04
varunchitturi <varunchitturi@icloud.com>
2020-06-26 07:57:03 -0700

    github actions

---
+++ .github/scripts/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -eo pipefail
+
+xcodebuild -workspace ../../Ghosting\ Connector.xcworkspace \
+            -scheme Ghosting\ Connector\
+            -destination platform=iOS\ Simulator,OS=13.5,name=iPhone\ 11 \
