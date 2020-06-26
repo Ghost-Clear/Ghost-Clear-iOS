fc186afb80a8d80ca61c746ea6f6a027e42cce24
varunchitturi <varunchitturi@icloud.com>
2020-06-26 08:06:01 -0700

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -2,6 +2,6 @@
 
 set -eo pipefail
 
-xcodebuild -workspace ../../Ghosting\ Connector.xcworkspace \
+xcodebuild -workspace /Ghosting\ Connector.xcworkspace\
             -scheme Ghosting\ Connector\
             -destination platform=iOS\ Simulator,OS=13.5,name=iPhone\ 11 \
