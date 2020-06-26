c54291cba48b6cd5d9533f77f5cd03e692f42a28
varunchitturi <varunchitturi@icloud.com>
2020-06-26 08:11:04 -0700

    remove faulty github actions

--- .github/scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -workspace /Ghosting-Connector/Ghosting\ Connector.xcworkspace\
-            -scheme Ghosting\ Connector\
-            -destination platform=iOS\ Simulator,OS=13.5,name=iPhone\ 11 \
