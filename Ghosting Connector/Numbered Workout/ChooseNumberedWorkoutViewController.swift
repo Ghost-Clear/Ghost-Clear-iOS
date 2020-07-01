aafb339a249d91efbe3e28016a02425d1734c997
varunchitturi <varunchitturi@icloud.com>
2020-07-01 05:23:50 -0700

    Update constraints and choose pattern protocol.

--- Ghosting Connector/Numbered Workout/ChooseNumberedWorkoutViewController.swift
+++ Ghosting Connector/Numbered Workout/ChooseNumberedWorkoutViewController.swift
@@ -15,52 +15,50 @@ class ChooseNumberedWorkoutViewController: UIViewController {
 	var isCL = false
 	var isLL = false
 	var isLR = false
+	var FLcount = 0
+	var FRcount = 0
+	var CLcount = 0
+	var CRcount = 0
+	var LLcount = 0
+	var LRcount = 0
 	var isRandomized = true
 	var minutesOff : Int!
 	var secondsOff : Int!
 	var numSets: Int!
 	var numGhosts : Int!
 	var count = 0
-	@IBOutlet weak var FLLabel: UILabel!
-	@IBOutlet weak var FRLabel: UILabel!
-	@IBOutlet weak var CLLabel: UILabel!
-	@IBOutlet weak var CRLabel: UILabel!
-	@IBOutlet weak var LLLabel: UILabel!
-	@IBOutlet weak var LRLabel: UILabel!
+	@IBOutlet weak var gridImage: UIImageView!
+	@IBOutlet weak var orderLabel: UILabel!
 	override func viewDidLoad() {
         super.viewDidLoad()
-		frontRight.setBackgroundImage(nil, for: .normal)
-		frontLeft.setBackgroundImage(nil, for: .normal)
-		centerRight.setBackgroundImage(nil, for: .normal)
-		centerLeft.setBackgroundImage(nil, for: .normal)
-		lowerLeft.setBackgroundImage(nil, for: .normal)
-		lowerRight.setBackgroundImage(nil, for: .normal)
-		FLLabel.text = ""
-		FRLabel.text = ""
-		CLLabel.text = ""
-		CRLabel.text = ""
-		LLLabel.text = ""
-		LRLabel.text = ""
+		gridImage.image = UIImage(named: "000000")
+		if isRandomized{
+			orderLabel.isHidden = true
+		}
+		else{
+			orderLabel.isHidden = false
+			orderLabel.text = ""
+		}
     }
 	@IBOutlet var frontLeft: UIButton!
 	func redoNumbers(number : Int){
-		if frontLeft.currentBackgroundImage != nil && (FLLabel.text! as NSString).integerValue > number {
-			FLLabel.text = String((FLLabel.text! as NSString).integerValue - 1)
+		if isFL && FLcount > number {
+			FLcount -= 1
 		}
-		if frontRight.currentBackgroundImage != nil && (FRLabel.text! as NSString).integerValue > number{
-			FRLabel.text = String((FRLabel.text! as NSString).integerValue - 1)
+		if isFR && FRcount > number{
+			FRcount -= 1
 		}
-		if centerRight.currentBackgroundImage != nil && (CRLabel.text! as NSString).integerValue > number{
-			CRLabel.text = String((CRLabel.text! as NSString).integerValue - 1)
+		if isCR && CRcount > number{
+			CRcount -= 1
 		}
-		if centerLeft.currentBackgroundImage != nil && (CLLabel.text! as NSString).integerValue > number{
-			CLLabel.text = String((CLLabel.text! as NSString).integerValue - 1)
+		if isCL && CLcount > number{
+			CLcount -= 1
 		}
-		if lowerLeft.currentBackgroundImage != nil && (LLLabel.text! as NSString).integerValue > number{
-			LLLabel.text = String((LLLabel.text! as NSString).integerValue - 1)
+		if isLL && LLcount > number{
+			LLcount -= 1
 		}
-		if lowerRight.currentBackgroundImage != nil && (LRLabel.text! as NSString).integerValue > number{
-			LRLabel.text = String((LRLabel.text! as NSString).integerValue - 1)
+		if isLR && LRcount > number{
+			LRcount -= 1
 		}
 	}
 	@IBAction func resetSelection(_ sender: Any) {
@@ -70,70 +68,192 @@ class ChooseNumberedWorkoutViewController: UIViewController {
 		isCL = false
 		isLR = false
 		isLL = false
-		frontRight.setBackgroundImage(nil, for: .normal)
-		frontLeft.setBackgroundImage(nil, for: .normal)
-		centerRight.setBackgroundImage(nil, for: .normal)
-		centerLeft.setBackgroundImage(nil, for: .normal)
-		lowerLeft.setBackgroundImage(nil, for: .normal)
-		lowerRight.setBackgroundImage(nil, for: .normal)
-		FLLabel.text = ""
-		FRLabel.text = ""
-		CLLabel.text = ""
-		CRLabel.text = ""
-		LLLabel.text = ""
-		LRLabel.text = ""
-		
+		gridImage.image = UIImage(named: "000000")
 		count = 0
 	}
 	@IBAction func FL(_ sender: Any) {
 		if isFL{
-			frontLeft.setBackgroundImage(nil, for: .normal)
 			isFL = false
 			count -= 1
-			redoNumbers(number: (FLLabel.text! as NSString).integerValue)
-			FLLabel.text = ""
+			redoNumbers(number: FLcount)
+			FLcount = 0
+			
 		}
 		else{
 			isFL = true
-			frontLeft.setBackgroundImage(UIImage(named: "Ellipse 15"), for: .normal)
 			count += 1
 			if !isRandomized{
-				FLLabel.text = String(count)}
+				FLcount = count
+			}
+			
+			}
+		var getImage : String! = ""
+		if isLR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isLL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isCR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isCL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isFR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isFL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		gridImage.image = UIImage(named: getImage)
+		if count != 0{
+			orderLabel.text = getOrderLabel()
+		}
+		else{
+			orderLabel.text = ""
 		}
 	}
 	@IBOutlet var frontRight: UIButton!
 	
 	@IBAction func FR(_ sender: Any) {
 		if isFR{
-			frontRight.setBackgroundImage(nil, for: .normal)
 			isFR = false
 			count -= 1
-			redoNumbers(number: (FRLabel.text! as NSString).integerValue)
-			FRLabel.text = ""
+			redoNumbers(number: FRcount)
+			FRcount = 0
 		}
 		else{
 			isFR = true
 			count += 1
-			frontRight.setBackgroundImage(UIImage(named: "Ellipse 15"), for: .normal)
 			if !isRandomized{
-				FRLabel.text = String(count)}
+				FRcount = count
+			}
+			
+			
+		}
+		var getImage : String! = ""
+		if isLR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isLL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isCR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isCL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isFR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isFL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		gridImage.image = UIImage(named: getImage)
+		if count != 0{
+			orderLabel.text = getOrderLabel()
+		}
+		else{
+			orderLabel.text = ""
 		}
 	}
-	
 	@IBAction func CL(_ sender: Any) {
 		if isCL{
-			centerLeft.setBackgroundImage(nil, for: .normal)
 			isCL = false
 			count -= 1
-			redoNumbers(number: (CLLabel.text! as NSString).integerValue)
-			CLLabel.text = ""
+			redoNumbers(number: CLcount)
+			CLcount = 0
 		}
 		else{
 			isCL = true
-			centerLeft.setBackgroundImage(UIImage(named: "Ellipse 15"), for: .normal)
 			count += 1
 			if !isRandomized{
-				CLLabel.text = String(count)}
+				CLcount = count
+			}
+			
+		}
+		var getImage : String! = ""
+		if isLR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isLL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isCR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isCL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isFR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isFL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		
+		gridImage.image = UIImage(named: getImage)
+		if count != 0{
+			orderLabel.text = getOrderLabel()
+		}
+		else{
+			orderLabel.text = ""
 		}
 	}
 	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -143,36 +263,127 @@ class ChooseNumberedWorkoutViewController: UIViewController {
 	@IBOutlet var centerLeft: UIButton!
 	@IBAction func LL(_ sender: Any) {
 		if isLL{
-			lowerLeft.setBackgroundImage(nil, for: .normal)
-			count -= 1
-			redoNumbers(number: (LLLabel.text! as NSString).integerValue)
 			isLL = false
-			LLLabel.text = ""
+			count -= 1
+			redoNumbers(number: LLcount)
+			LLcount = 0
 		}
 		else{
 			isLL = true
-			lowerLeft.setBackgroundImage(UIImage(named: "Ellipse 15"), for: .normal)
 			count += 1
 			if !isRandomized{
-				LLLabel.text = String(count)}
-		}	}
+				LLcount = count
+			}
+			
+			
+		}
+		var getImage : String! = ""
+		if isLR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isLL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isCR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isCL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isFR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isFL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		gridImage.image = UIImage(named: getImage)
+		if count != 0{
+			orderLabel.text = getOrderLabel()
+		}
+		else{
+			orderLabel.text = ""
+		}
+	}
 	
 	
 	@IBOutlet var centerRight: UIButton!
 	@IBAction func CR(_ sender: Any) {
 		if isCR{
-			centerRight.setBackgroundImage(nil, for: .normal)
 			isCR = false
 			count -= 1
-			redoNumbers(number: (CRLabel.text! as NSString).integerValue)
-			CRLabel.text = ""
+			redoNumbers(number: CRcount)
+			CRcount = 0
 		}
 		else{
 			isCR = true
-			centerRight.setBackgroundImage(UIImage(named: "Ellipse 15"), for: .normal)
 			count += 1
 			if !isRandomized{
-				CRLabel.text = String(count)}
+				CRcount = count
+			}
+			
+			
+		}
+		var getImage : String! = ""
+		if isLR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isLL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isCR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isCL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isFR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isFL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		gridImage.image = UIImage(named: getImage)
+		if count != 0{
+			orderLabel.text = getOrderLabel()
+		}
+		else{
+			orderLabel.text = ""
 		}
 	}
 	
@@ -182,19 +393,65 @@ class ChooseNumberedWorkoutViewController: UIViewController {
 	@IBOutlet var lowerRight: UIButton!
 	@IBAction func LR(_ sender: Any) {
 		if isLR{
-			lowerRight.setBackgroundImage(nil, for: .normal)
 			isLR = false
 			count -= 1
-			redoNumbers(number: (LRLabel.text! as NSString).integerValue)
-			LRLabel.text = ""
+			redoNumbers(number: LRcount)
+			LRcount = 0
 		}
 		else{
 			isLR = true
-			lowerRight.setBackgroundImage(UIImage(named: "Ellipse 15"), for: .normal)
 			count += 1
 			if !isRandomized{
-				LRLabel.text = String(count)}
+				LRcount = count
+			}
+			
 		}
+		var getImage : String! = ""
+		if isLR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isLL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isCR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isCL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isFR{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		if isFL{
+			getImage += "1"
+		}
+		else{
+			getImage += "0"
+		}
+		
+		gridImage.image = UIImage(named: getImage)
+		if count != 0{
+			orderLabel.text = getOrderLabel()
+		}
+		else{
+			orderLabel.text = ""
+		}
+		
 	}
 	@IBAction func startGhost(_ sender: Any) {
 		if !isFR && !isFL && !isCR && !isCL && !isLR && !isLL{
@@ -215,8 +472,88 @@ class ChooseNumberedWorkoutViewController: UIViewController {
 		self.navigationController?.popViewController(animated: true)
 	}
 	
-    // MARK: - Navigation
-
+	func getOrderLabel() -> String{
+		let o = getOrderArr()
+		var toReturn = ""
+		var count = 1
+		for i in o{
+			if i == "FR"{
+				toReturn += String(count) + ". " + "Front Right" + " "
+			}
+			if i == "FL"{
+				toReturn += String(count) + ". " + "Front Left" + " "
+			}
+			if i == "CR"{
+				toReturn += String(count) + ". " + "Center Right" + " "
+			}
+			if i == "CL"{
+				toReturn += String(count) + ". " + "Center Left" + " "
+			}
+			if i == "LR"{
+				toReturn += String(count) + ". " + "Back Right" + " "
+			}
+			if i == "LL"{
+				toReturn += String(count) + ". " + "Back Left" + " "
+			}
+			count += 1
+		}
+		return toReturn
+	}
+	
+	func getOrderArr() -> [String]{
+		var order : [String]! = []
+		for _ in 0...count-1 {
+			order.append("")
+		}
+		if !isRandomized{
+			if isFR{
+				order[FRcount-1] = "FR"
+			}
+			if isFL{
+				order[FLcount-1] = "FL"
+			}
+			if isCL{
+				order[CLcount-1] = "CL"
+			}
+			if isCR{
+				order[CRcount-1] = "CR"
+			}
+			if isLR{
+				order[LRcount-1] = "LR"
+			}
+			if isLL{
+				order[LLcount-1] = "LL"
+			}
+		}
+		else {
+			var which = 0
+			if isFR{
+				order[which] = "FR"
+				which += 1
+			}
+			if isFL{
+				order[which] = "FL"
+				which += 1
+			}
+			if isCR{
+				order[which] = "CR"
+				which += 1
+			}
+			if isCL{
+				order[which] = "CL"
+				which += 1
+			}
+			if isLR{
+				order[which] = "LR"
+				which += 1
+			}
+			if isLL{
+				order[which] = "LL"
+				which += 1
+			}
+		}
+		return order
+	}
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
@@ -238,59 +575,10 @@ class ChooseNumberedWorkoutViewController: UIViewController {
 				childVC.LR = isLR
 				childVC.LL = isLL
 				childVC.ghostsToDo = numGhosts
-				var order : [String]! = []
-				for _ in 0...count-1 {
-					order.append("")
-				}
-				if !isRandomized{
-					if FRLabel.text != "" && frontRight.currentBackgroundImage != nil{
-						order[((FRLabel.text!) as NSString).integerValue-1] = "FR"
-					}
-					if FLLabel.text != "" && frontLeft.currentBackgroundImage != nil{
-						order[((FLLabel.text!) as NSString).integerValue-1] = "FL"
-					}
-					if CLLabel.text != "" && centerLeft.currentBackgroundImage != nil{
-						order[((CLLabel.text!) as NSString).integerValue-1] = "CL"
-					}
-					if CRLabel.text != "" && centerRight.currentBackgroundImage != nil{
-						order[((CRLabel.text!) as NSString).integerValue-1] = "CR"
-					}
-					if LRLabel.text != "" && lowerRight.currentBackgroundImage != nil{
-						order[((LRLabel.text!) as NSString).integerValue-1] = "LR"
-					}
-					if LLLabel.text != "" && lowerLeft.currentBackgroundImage != nil{
-						order[((LLLabel.text!) as NSString).integerValue-1] = "LL"
-					}
-				}
-				else {
-					var which = 0
-					if isFR{
-						order[which] = "FR"
-						which += 1
-					}
-					if isFL{
-						order[which] = "FL"
-						which += 1
-					}
-					if isCR{
-						order[which] = "CR"
-						which += 1
-					}
-					if isCL{
-						order[which] = "CL"
-						which += 1
-					}
-					if isLR{
-						order[which] = "LR"
-						which += 1
-					}
-					if isLL{
-						order[which] = "LL"
-						which += 1
-					}
-				}
-				print(order!)
-				childVC.order = order
+				var o : [String]! = []
+				o = getOrderArr()
+				print(o!)
+				childVC.order = o
 				
 			}
 		}
